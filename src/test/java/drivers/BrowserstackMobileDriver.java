45a77ee0d27092a5721fbe675daafea11c2fdeee
Zyumalin Dmitriy <zyumalin@bk.ru>
2021-08-25 09:19:11 +0300

    added url to current apk and capabilites

--- src/test/java/drivers/BrowserstackMobileDriver.java
+++ src/test/java/drivers/BrowserstackMobileDriver.java
@@ -7,8 +7,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import javax.annotation.Nonnull;
+import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class BrowserstackMobileDriver implements WebDriverProvider {
     @Nonnull
@@ -17,6 +20,7 @@ public class BrowserstackMobileDriver implements WebDriverProvider {
         capabilities.setCapability("browserstack.user", Project.deviceConfig.hubUsername());
         capabilities.setCapability("browserstack.key", Project.deviceConfig.hubPassword());
         capabilities.setCapability("app", Project.deviceConfig.appBs());
+        capabilities.setCapability("app_url", Project.deviceConfig.appUrl());
         capabilities.setCapability("device", Project.deviceConfig.device());
         capabilities.setCapability("os_version", Project.deviceConfig.platformVersion());
         capabilities.setCapability("project", Project.deviceConfig.bsProject());
